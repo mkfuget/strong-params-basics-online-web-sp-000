5133bbdb3b79677143313edc5e66fcbfee1dc396
Michael Fuget <michaelfuget@gmail.com>
2021-01-23 02:01:21 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,8 +29,8 @@ class PostsController < ApplicationController
 
 	private
 
-	def post_params(*args)
-	  params.require(:post).permit(*args)
+	def post_params
+	  params.require(:post).permit(:title, :description)
 	end
 
 end
